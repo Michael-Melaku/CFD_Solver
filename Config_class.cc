1791b3bfe792e6250e05ae66ca65d9de1ee807ae
Taye <13236935+Michael-Melaku@users.noreply.github.com>
2024-12-04 17:38:10 -0500

    Create Config_class.cc

---
+++ Config_class.cc
@@ -0,0 +1,13 @@
+class SOLVER_PARAMETERS 
+{
+public:
+
+int NUM_X_CELLS = 100;
+int NUM_GHOST_CELLS = 3;
+int NUM_RK_STEPS = 1;
+int MAX_TIME_ITERATION = 100000
+double STOPPING_TIME = 2;
+doble MIN_X = -1;
+double MAX_X = 1
+
+}
